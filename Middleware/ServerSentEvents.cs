eafae50b1b74d406d3d8aabc79b7a3d183d62d28
Colin Campbell <coder@colin.geek.nz>
2017-04-01 09:33:58 +1300

    No need to expose the middleware implementation

--- Middleware/ServerSentEvents.cs
+++ Middleware/ServerSentEvents.cs
@@ -15,61 +15,61 @@ namespace PoorMan.SSE.Middleware {
         }
 
         public static IApplicationBuilder UseServerSentEvents(this IApplicationBuilder builder) {
-            return builder.UseMiddleware<ServerSentEventsMiddleware>();
-        }
-    }
-
-    public class ServerSentEventsMiddleware {
-
-        private readonly RequestDelegate _next;
-
-        private readonly ConcurrentDictionary<Guid, BlockingCollection<string>> _connections = new ConcurrentDictionary<Guid, BlockingCollection<string>>();
-
-        public ServerSentEventsMiddleware(RequestDelegate next) {
-            _next = next;
+            return builder.UseMiddleware<Middleware>();
         }
 
-        public async Task Invoke(HttpContext context) {
+        private class Middleware {
 
-            // This is our endpoint, should be configurable
-            if ("/sse" == context.Request.Path.ToString() && "GET" == context.Request.Method) {
+            private readonly RequestDelegate _next;
 
-                // This is SSE so set the correct content type
-                context.Response.Headers.Add("Content-Type", "text/event-stream");
+            private readonly ConcurrentDictionary<Guid, BlockingCollection<string>> _connections = new ConcurrentDictionary<Guid, BlockingCollection<string>>();
 
-                // Generate connection id
-                var id = Guid.NewGuid();
-
-                // Subscribe for notifications
-                var queue = new BlockingCollection<string>();
-                _connections[id] = queue;
-
-                // Keep streaming out notifications until client disconnects
-                try { while (!context.RequestAborted.IsCancellationRequested) {
-                    // Wait for a notification
-                    var message = queue.Take(context.RequestAborted);
-
-                    // Send the notification to the client
-                    await context.Response.WriteAsync($"data: {message}\r\r", context.RequestAborted);
-                    await context.Response.Body.FlushAsync(context.RequestAborted);
-                }
-
-                // Always unsubscribe
-                } finally {
-                    _connections.TryRemove(id, out queue);
-                }
-
-            // This is our endpoint, should be configurable
-            } else if ("/sse" == context.Request.Path.ToString() && "POST" == context.Request.Method) {
-
-                foreach(var connection in _connections.Select(item => item.Value)) {
-                    connection.Add("ping/pong");
-                }
-
-            // Nothing to do here, move along.
-            } else {
-                await _next(context);
+            public Middleware(RequestDelegate next) {
+                _next = next;
             }
-        }        
+
+            public async Task Invoke(HttpContext context) {
+
+                // This is our endpoint, should be configurable
+                if ("/sse" == context.Request.Path.ToString() && "GET" == context.Request.Method) {
+
+                    // This is SSE so set the correct content type
+                    context.Response.Headers.Add("Content-Type", "text/event-stream");
+
+                    // Generate connection id
+                    var id = Guid.NewGuid();
+
+                    // Subscribe for notifications
+                    var queue = new BlockingCollection<string>();
+                    _connections[id] = queue;
+
+                    // Keep streaming out notifications until client disconnects
+                    try { while (!context.RequestAborted.IsCancellationRequested) {
+                        // Wait for a notification
+                        var message = queue.Take(context.RequestAborted);
+
+                        // Send the notification to the client
+                        await context.Response.WriteAsync($"data: {message}\r\r", context.RequestAborted);
+                        await context.Response.Body.FlushAsync(context.RequestAborted);
+                    }
+
+                    // Always unsubscribe
+                    } finally {
+                        _connections.TryRemove(id, out queue);
+                    }
+
+                // This is our endpoint, should be configurable
+                } else if ("/sse" == context.Request.Path.ToString() && "POST" == context.Request.Method) {
+
+                    foreach(var connection in _connections.Select(item => item.Value)) {
+                        connection.Add("ping/pong");
+                    }
+
+                // Nothing to do here, move along.
+                } else {
+                    await _next(context);
+                }
+            }
+        }
     }
 }
